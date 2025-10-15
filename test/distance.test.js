f6626b91629b8812b913cdf9f8f9a09c06a646d0
Phukrit Kittinontana <phukrit7171@gmail.com>
2025-10-15 19:26:03 +0700

    fix jest

--- test/distance.test.js
+++ test/distance.test.js
@@ -1,5 +1,8 @@
 import convertDistance from "../src/distance.js";
+import { test, expect, describe} from '@jest/globals';
 
-test("converts 1000 meters to 1 kilometer", () => {
-  expect(convertDistance(1000)).toBe(1);
+describe("Distance Conversion", () => {
+  test("converts 1000 meters to 1 kilometer", () => {
+    expect(convertDistance(1000)).toBe(1);
+  });
 });
--- test/weight.test.js
+++ test/weight.test.js
@@ -1,5 +1,9 @@
+import { test, expect, describe} from '@jest/globals';
 import convertWeight from "../src/weight.js";
 
-test("converts 5000 grams to 5 kilograms", () => {
-  expect(convertWeight(5000)).toBe(5);
+describe("Weight Conversion", () => {
+  test("converts 5000 grams to 5 kilograms", () => {
+    expect(convertWeight(5000)).toBe(5);
+  });
 });
+
